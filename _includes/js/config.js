f8a354c7da0b3dbb13c5b2cd73cd885c6b7ece95
Morten Høfft <mhoefft@gbif.org>
2020-10-14 13:10:57 +0200

    scope to all

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -9,14 +9,4 @@ if (primaryColor) {
   }});
 }
 
-var leaf1 = "POLYGON((-126.90857 -32.29178,-97.60803 -33.32135,-54.40979 -47.11126,7.73987 -52.25471,101.15112 -47.50236,163.48755 -27.0102,114.71924 12.20044,28.30627 25.9778,-50.44922 -0.48339,-35.39795 50.04656,-65.8905 82.3806,-84.57825 78.93455,-98.44299 50.88224,-73.65784 0.56579,-126.90857 -32.29178))";
-var leaf2 = "POLYGON((-20.4895 34.12999,41.56128 45.72536,80.97473 80.29422,-8.46497 65.28199,-20.4895 34.12999))";
-var siteConfig = {
-  rootPredicate: {
-    type: 'and', predicates: [
-      { key: 'mediaTypes', type: 'equals', value: 'StillImage'},
-      { key :'hasCoordinate', type: 'equals', value: true},
-      { key :'basisOfRecord', type: 'in', values: ['PRESERVED_SPECIMEN', 'MATERIAL_SAMPLE']},
-    ]
-  }
-};
+var siteConfig = {};
