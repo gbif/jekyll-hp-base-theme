bb59f461ccefd26c4c8130ea23fc70fb11ceb238
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2025-12-05 10:36:06 +0100

    use local copy instead of fallback to another cloudflare react

---
+++ assets/theme/js/react-dom.js
@@ -0,0 +1,6 @@
+---
+layout: null
+# This page is now processed because it contains frontmatter
+# That means that we can use Liquid and include variables from config
+---
+{% include js/react-dom.js %}
